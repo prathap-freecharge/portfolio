9b94f4c145c07da353a9c81a4ac9bf54ef3eeccb
prathap-freecharge <prathap.moola@freecharge.com>
2024-10-13 17:33:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/portfolio', // Add the path where your site is hosted
 };
 
 export default nextConfig;
